255f7a5b0239011cd21cea3ffa25ac7e0049766a
Anna Startseva <an.startseva@innopolis.university>
2021-08-22 16:05:30 +0300

    Simple Python App

---
+++ lab1/app_python/main.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+import pytz as pytz
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    time_zone = pytz.timezone("Europe/Moscow")
+    time = datetime.now(time_zone).strftime('%H:%M:%S')
+    return render_template('index.html', time=time)
+
+
+app.run(host="127.0.0.1")
+
+
